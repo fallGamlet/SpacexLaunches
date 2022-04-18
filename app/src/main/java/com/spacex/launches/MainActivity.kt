9633d87e5f33537d373ac4cc9455f13677a9d02c
fallGamlet <fallGamlet@yandex.ru>
2022-04-18 10:52:02 +0300

    add usage screen on MainActivity

--- app/src/main/java/com/spacex/launches/MainActivity.kt
+++ app/src/main/java/com/spacex/launches/MainActivity.kt
@@ -2,11 +2,22 @@ package com.spacex.launches
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commitNow
+import com.spacex.launches.ui.launches.preview.LaunchesPreviewFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationProvider {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.commitNow {
+                replace(R.id.container, LaunchesPreviewFragment.newInstance())
+            }
+        }
+    }
+
+    override fun openLaunchDetails(launchId: String) {
+
     }
 }
